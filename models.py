cc60f373e1a306dff4dd921e9c92295847087e75
HenryHai0407 <hoanghai15100@gmail.com>
2025-04-09 23:59:14 +0300

    update models.py file

--- models.py
+++ models.py
@@ -2,4 +2,14 @@ from sqlalchemy import Column, Integer, String
 from .main import Base
 
 class User(Base):
-    
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True,index=True)
+    username = Column(String(50, unique=True, index=True))
+    hashed_password = Column(String(100))
+
+class Order(Base):
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    product_id = Column(Integer)
+    quantity = Column(Integer)

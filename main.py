574d15c658f238c223eda8530a6eca241b928401
HenryHai0407 <hoanghai15100@gmail.com>
2025-04-18 11:07:04 +0300

    add routers folder for grouping all routers in the same file

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 # main.py
 from fastapi import FastAPI
-from controllers import category_controller, product_controller, order_controller, auth_controller, user_controller
+from routers.router import routes
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 
 app = FastAPI(title="FastAPI Store")
 
-app.include_router(category_controller.router)
-app.include_router(product_controller.router)
-app.include_router(order_controller.router)
-app.include_router(auth_controller.router)
-app.include_router(user_controller.router)
+app.include_router(routes)
 
 @app.get("/")
 async def index(request: Request):
